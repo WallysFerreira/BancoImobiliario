3f4de56e2f6be282a73d29a687f677be32ca6947
Wallys Ferreira <wallysfsilva@gmail.com>
2023-08-28 14:07:24 -0300

    adicionar cor pra cada jogador

--- app/src/main/java/com/example/cartaoimobiliario/MainActivity.kt
+++ app/src/main/java/com/example/cartaoimobiliario/MainActivity.kt
@@ -5,30 +5,47 @@ package com.example.cartaoimobiliario
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Camera
 import androidx.compose.material.icons.rounded.QrCode
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.cartaoimobiliario.ui.theme.CartaoImobiliarioTheme
+
+val cores = listOf(Color.Black, Color.White, Color.Blue, Color.Yellow, Color.Red)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,12 +58,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JogadorForm(
+    cor: Color,
     index: Int,
     modifier: Modifier = Modifier,
 ) {
     Row (
         verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
         modifier = modifier
+            .fillMaxWidth()
     ) {
         TextField(
             value = "",
@@ -55,14 +75,18 @@ fun JogadorForm(
             trailingIcon = {
                 Button(
                     onClick = { /* Escolher cor */ },
-                    modifier = Modifier.padding(end = 8.dp)
+                    modifier = Modifier.padding(end = 8.dp),
+                    colors = ButtonDefaults.buttonColors(cor),
                 ) {
                     Text("")
                 }
            },
+            modifier = Modifier.width(330.dp),
         )
         IconButton(
             onClick = {},
+            modifier = Modifier
+                .padding(4.dp)
         ) {
             Icon(Icons.Rounded.QrCode, contentDescription = null)
         }
@@ -71,14 +95,28 @@ fun JogadorForm(
 
 @Composable
 fun InserirJogadores(
-    qntJogadores: Int = 2,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(
+    var qntJogadores by remember { mutableStateOf(2) }
 
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()
     ) {
-        items(qntJogadores) { index ->
-            JogadorForm(index = index + 1, modifier = Modifier.padding(bottom = 8.dp))
+        for (i in 1..qntJogadores) {
+            JogadorForm(index = i, modifier = Modifier.padding(bottom = 8.dp), cor = cores[i - 1])
+        }
+
+        if (qntJogadores < 5) {
+            IconButton(
+                onClick = {
+                    qntJogadores++
+                },
+                colors = IconButtonDefaults.iconButtonColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
+                modifier = Modifier.clip(CircleShape)
+            ) {
+                Icon(Icons.Rounded.Add, contentDescription = null)
+            }
         }
     }
 }
@@ -86,17 +124,18 @@ fun InserirJogadores(
 @Composable
 fun CartaoImobiliarioApp() {
     CartaoImobiliarioTheme {
+        InserirJogadores()
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun JogadorFormPreview() {
-    JogadorForm(1)
+    JogadorForm(index = 1, cor = cores[1])
 }
 
 @Preview(showBackground = true)
 @Composable
 fun InserirJogadoresPreview() {
-    InserirJogadores(2)
+    InserirJogadores()
 }
